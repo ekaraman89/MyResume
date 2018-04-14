e7a91ec91553cfaa711fd62e4ed1893473f44fc5
emrah karaman <ekaraman89@hotmail.com>
2018-04-14 21:31:02 +0300

    routeconfig updated

--- MyResume/App_Start/RouteConfig.cs
+++ MyResume/App_Start/RouteConfig.cs
@@ -13,11 +13,13 @@ namespace MyResume
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Home", "", new { controller = "Home", action = "Index" });
+            routes.MapRoute("Profile", "", new { controller = "Home", action = "Profile" });
+            routes.MapRoute("Resume", "", new { controller = "Home", action = "Resume" });
+            routes.MapRoute("Portfolio", "", new { controller = "Home", action = "Portfolio" });
+            routes.MapRoute("Blog", "", new { controller = "Home", action = "Blog" });
+            routes.MapRoute("Contact", "", new { controller = "Home", action = "Contact" });
+
         }
     }
 }
